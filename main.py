36259fa00c5e02fed59eebe680745894a467812d
BOUCHERET <webmasterdu63@gmail.com>
2026-05-23 11:46:14 +0200

    Add audit_systeme function to the main menu

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from scan_rapide import scan_rapide
 from scan_complet import scan_complet
+from audit_systeme import audit_systeme
 
 def menu():
     while True:
@@ -8,7 +9,7 @@ def menu():
         print("===========================================\n")
         print("1 - Scan rapide")
         print("2 - Scan complet")
-        print("3 - Audit système (bientôt)")
+        print("3 - Audit système")
         print("4 - Scan réseau local (bientôt)")
         print("5 - Générer un rapport HTML (bientôt)")
         print("0 - Quitter\n")
@@ -19,6 +20,8 @@ def menu():
             scan_rapide()
         elif choix == "2":
             scan_complet()
+        elif choix == "3":
+            audit_systeme()
         elif choix == "0":
             print("Fermeture de CyberScan2026.")
             break
@@ -27,3 +30,4 @@ def menu():
 
 if __name__ == "__main__":
     menu()
+
